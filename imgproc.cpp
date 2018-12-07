2090c4b9c40442639c33cd0d45bc575082a11bac
lolstatbeta <ojs0728@kangwon.ac.kr>
2018-12-07 10:47:15 +0900

    assignment2_2.cpp - flood_fill Struct_Point, Queue.front.pop

--- imgproc.cpp
+++ imgproc.cpp
@@ -1,4 +1,5 @@
 #include "imgproc.h"
+#include <queue>
 
 namespace IPCVL {
 	namespace IMG_PROC {
@@ -148,6 +149,131 @@ namespace IPCVL {
 			}
 
 			thresh_binary(inputMat, outputProb, threshold);
+		}
+
+		void flood_fill(cv::InputArray src, cv::OutputArray dst, const UTIL::CONNECTIVITIES & direction)
+		{
+			cv::Mat inputMat = src.getMat();
+			//I로 복사
+			dst.create(inputMat.size(), CV_32SC1);
+			cv::Mat l = dst.getMat();
+			l.setTo(cv::Scalar(0.));
+
+
+			// -1은 아직 번호를 안붙였음을 의미
+			for (int y = 0; y < l.rows; y++) {
+				for (int x = 0; x < l.cols; x++) {
+					// I의 경계인 화소를 0으로 설정한다 - 영상이 밖으로 나가는 것을 방지
+					if (y == 0 || y == l.rows - 1 || x == 0 || x == l.cols - 1)
+						l.at<int>(y, x) = 0;
+					else if (inputMat.at<uchar>(y, x) != 0)
+						l.at<int>(y, x) = -1;
+				}
+			}
+
+			int label = 1;
+
+			
+			for (int y = 1; y < l.rows - 1; y++) {
+				for (int x = 1; x < l.cols - 1; x++) {
+					if (l.at<int>(y, x) == -1)
+					{
+						if (direction == 0)
+						{
+							flood_fill4(l, y, x, label);
+							label++;
+						}
+						else if(direction == 1)
+						{
+							flood_fill8(l, y, x, label);
+							label++;
+						}
+						else if (direction == 2)
+						{
+							efficient_flood_fill4(l, y, x, label);
+							label++;
+						}
+					}
+				}
+			}
+
+		}
+
+
+		void flood_fill4(cv::Mat & l, const int & j, const int & i, const int & label)
+		{
+			
+			if (l.at<int>(j, i) == -1) {
+				l.at<int>(j, i) = label;
+				
+				flood_fill4(l, j, i+1, label); //east
+				flood_fill4(l, j-1, i, label); //north
+				flood_fill4(l, j, i - 1, label); //west
+				flood_fill4(l, j+1, i, label); //south
+			}
+		}
+		void flood_fill8(cv::Mat & l, const int & j, const int & i, const int & label)
+		{
+			if (l.at<int>(j, i) == -1) {
+				l.at<int>(j, i) = label;
+
+				flood_fill4(l, j - 1, i - 1, label); 
+				flood_fill4(l, j - 1, i, label); 
+				flood_fill4(l, j - 1, i + 1, label); 
+				
+				flood_fill4(l, j, i - 1, label); 
+				flood_fill4(l, j, i + 1, label); 
+			
+			
+				flood_fill4(l, j + 1, i - 1, label);
+				flood_fill4(l, j + 1, i, label);
+				flood_fill4(l, j + 1, i + 1, label); 
+			}
+		}
+		
+		typedef struct Point
+		{
+			int y;
+			int x;
+
+			Point(int y, int x) : y(y), x(x) {}
+			/*{
+				this->y = y;
+				this->x = x;
+			}*/
+		};
+
+		void efficient_flood_fill4(cv::Mat & l, const int & j, const int & i, const int & label)
+		{
+			std::queue<Point> Q;
+			Q.push(Point(j,i));
+
+			int x=0, y=0;
+			while (!Q.empty())
+			{
+			
+				y = Q.front().y;
+				x = Q.front().x;
+				Q.pop();
+
+				if (l.at<int>(y, x) == -1)
+				{
+					int left = x;
+					int right = x;
+					while (l.at<int>(y, left - 1) == -1)left--;
+					while (l.at<int>(y, right + 1) == -1)right++;
+
+					for (int c = left; c < right; c++)
+					{
+						l.at<int>(y, c) = label;
+						if (l.at<int>(y - 1, c) == -1 && (c == left || l.at<int>(y - 1, c - 1) != -1))
+							Q.push(Point(y-1, c));
+						if (l.at<int>(y + 1, c) == -1 && (c == left || l.at<int>(y + 1, c - 1) != -1))
+							Q.push(Point(y+1, c));
+					}
+				}
+			}
+
 		}
 	}  // namespace IMG_PROC
 
